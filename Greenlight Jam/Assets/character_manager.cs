ed7e9df9ab45c5b97bd850f913a59d5092715672
Princex Eli <31641573+princexEli@users.noreply.github.com>
2022-05-23 19:38:52 -0400

    Created moveable character

---
+++ Greenlight Jam/Assets/character_manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class character_manager : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if(spriteRenderer.flipX && horizontalInput > 0)
+		{
+            spriteRenderer.flipX = false;
+        }
+        else if(!spriteRenderer.flipX && horizontalInput < 0)
+		{
+            spriteRenderer.flipX = true;
+        }
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+        animator.SetFloat("movement", Mathf.Abs(horizontalInput+ verticalInput));
+    }
+}
