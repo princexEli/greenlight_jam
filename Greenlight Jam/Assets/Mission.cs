45abaa12ac8f03e624f2adb4b8fc957739b2760d
Princex Eli <31641573+princexEli@users.noreply.github.com>
2022-05-24 21:56:15 -0400

    Wrote Mission Generation code and reorganized prefabs

---
+++ Greenlight Jam/Assets/Mission.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    string title = "Mission", giver = "Captain Ash";
+    bool isRequired;
+    bool isComplete
+	{
+		get
+		{
+            foreach(Mission_Component c in components)
+			{
+                if (!c.isComplete)
+                    return false;
+			}
+            return true;
+		}
+	}
+    Mission_Manager manager;
+    List<Mission_Component> components;
+
+	private void Start()
+	{
+        manager = Mission_Manager.Instance;
+    }
+
+	private class Mission_Component
+	{
+        string item;
+        int current = 0, required;
+        public bool isComplete
+		{
+			get
+			{
+                return current >= required;
+			}
+		}
+
+        public Mission_Component(string itemName, int requiredAmount)
+		{
+            required = requiredAmount;
+            item = itemName;
+        }
+	}
+
+	public Mission(bool isMain)
+	{
+        components = new List<Mission_Component>();
+        isRequired = isMain;
+        HashSet<string> componentItems = new HashSet<string>();
+
+        for (int i = 0; i < randomNum(manager.maxComponents); i++) 
+        {
+            components.Add(new Mission_Component(randomItem(componentItems), randomNum(manager.maxItems)));
+        }
+    }
+
+    private string randomItem(HashSet<string> itemsToExclude)
+    {
+        string item;
+        do {
+            item = manager.items[Random.Range(1, manager.items.Count - 1)];
+        } while (!itemsToExclude.Contains(item));
+        itemsToExclude.Add(item);
+        return item;
+    }
+
+    private static int randomNum(int max)
+	{
+        return Random.Range(1, max);
+    }
+}
---
+++ Greenlight Jam/Assets/Mission_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission_Manager : MonoBehaviour
+{
+    #region instance
+    private static Mission_Manager instance;
+    public static Mission_Manager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Mission_Manager>();
+            }
+
+            return instance;
+        }
+        set { instance = value; }
+    }
+    #endregion
+
+    public List<string> items;
+    List<Mission> activeMissions;
+
+    public int maxComponents = 3, maxItems = 5;
+
+	private void Start()
+	{
+		if(maxComponents > items.Count)
+		{
+            Debug.LogError("maxComponent will be set to items.Count, since the value entered will create an infinate loop");
+            maxComponents = items.Count;
+		}
+
+        activeMissions = new List<Mission>();
+	}
+}
