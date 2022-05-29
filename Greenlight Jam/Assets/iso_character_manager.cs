f092e6e2efcebd25a81627a694295927fa751bcb
Princex Eli <31641573+princexEli@users.noreply.github.com>
2022-05-29 13:42:37 -0400

    Camera Update

---
+++ Greenlight Jam/Assets/iso_character_manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iso_character_manager : MonoBehaviour
+{
+    float moveSpeed = 5;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
+    }
+}
